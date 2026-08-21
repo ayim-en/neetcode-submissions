5ff223f934cd543817e234529f556b669ee96448
Ayimen Hussien <ayimen.dev@gmail.com>
2026-08-21 11:12:20 -0700

    Add: longest-consecutive-sequence - submission-2

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-2.py
@@ -0,0 +1,24 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        """
+        i int arr nums
+        o str len of longest consecutive sequence
+        c O(n) time complexity; elements are not always consecutive in nums
+        e a consequtive sequence is one where nums[i + 1] = i + 1 for every element
+        t O(N)
+        s
+        """
+        num_set = set(nums)
+        max_ctr = 0
+
+        for num in num_set:
+            if num - 1 not in num_set:
+                ctr = 1
+                
+                while num + ctr in num_set:
+                    ctr += 1
+                
+                max_ctr = max(ctr, max_ctr)
+
+        return max_ctr
+                
